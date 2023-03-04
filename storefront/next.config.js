bf559242125379c456769c7e6684465a00298631
Hamid <hamidtahir2000@gmail.com>
2023-03-04 23:54:35 +0500

    first

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -5,7 +5,11 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: [
+      "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      "localhost",
+      "honnest-gummies-server.s3.amazonaws.com",
+    ],
   },
 })
 
